08fca1bab0e32f164069fae1a2494c3ca8405ee0
Mike Wilkinson <mwilkinson@imo-online.com>
2018-09-03 17:40:27 -0500

    refactored qr scanning

--- src/client.ts
+++ src/client.ts
@@ -1,153 +1,117 @@
-import jsQR, { QRCode } from 'jsqr';
-import { Point } from 'jsqr/dist/locator';
-import { Room } from './models/room';
-import { Telephone } from './telephony/telephone';
-import { DrawingBoard } from './drawing-board';
-import { NewDrawing } from './models/events/new-drawing';
-import { Guess } from './models/events/guess';
-import { RoomState } from './models/events/stateChanged';
-import { Subject, Observable } from 'rxjs';
+import { Point } from "jsqr/dist/locator";
+import { Room } from "./models/room";
+import { Telephone } from "./telephony/telephone";
+import { DrawingBoard } from "./drawing-board";
+import { NewDrawing } from "./models/events/new-drawing";
+import { Guess } from "./models/events/guess";
+import { RoomState } from "./models/events/stateChanged";
+import { Subject, Observable } from "rxjs";
+import { Scanner } from "./scanner";
 
 let drawingBoard: DrawingBoard;
-const telephone: Telephone = new Telephone('tacoman');
-let video: HTMLVideoElement;
-let scanner: HTMLCanvasElement;
-let scannerContext: CanvasRenderingContext2D;
+let telephone: Telephone;
+let scanner: Scanner = new Scanner('scanner');
+
 let loadingMessage: HTMLElement;
+let userArea: HTMLElement;
+let scanningArea: HTMLElement;
+let playArea: HTMLElement;
 let guessArea: HTMLElement;
 let sendDrawing: HTMLElement;
 
+window.onload = () => {
+  initialize();
+};
+
 function initialize() {
-    guessArea = document.getElementById('guessArea');
-    drawingBoard = new DrawingBoard('drawingBoard');
-    video = document.createElement('video');
-    scanner = document.getElementById('scanner') as HTMLCanvasElement;
-    scannerContext = scanner.getContext('2d');
-    loadingMessage = document.getElementById('loadingMessage');
-    const connect = document.getElementById('connect');
-    sendDrawing = document.getElementById('sendDrawing');
-    connect.addEventListener('click', () => {
-        const idElement = document.getElementById('connectId') as HTMLInputElement;
-        const id = idElement.value;
-        joinRoom(new Room(id, 'butts'));
-    });   
-}
+  userArea = document.getElementById("userArea");
+  scanningArea = document.getElementById("scanningArea");
+  playArea = document.getElementById("playArea");
+  guessArea = document.getElementById("guessArea");
 
-function analyzeFrame() {
-    loadingMessage.innerText = 'Loading video...';
-    if (video.readyState !== video.HAVE_ENOUGH_DATA) {
-        return requestAnimationFrame(analyzeFrame);
-    }
+  drawingBoard = new DrawingBoard("drawingBoard");
+  loadingMessage = document.getElementById("loadingMessage");
+  const connect = document.getElementById("connect");
+  sendDrawing = document.getElementById("sendDrawing");
 
-    scanner.height = video.videoHeight;
-    scanner.width = video.videoWidth;
-    scannerContext.drawImage(video, 0, 0, scanner.width, scanner.height);
-    const imageData = scannerContext.getImageData(0, 0, scanner.width, scanner.height);
-    const code: QRCode = jsQR(imageData.data, imageData.width, imageData.height);
-    if (!code) {
-        return requestAnimationFrame(analyzeFrame);
-    }
+  const login = document.getElementById("login");
+  login.addEventListener("click", () => {
+    userArea.setAttribute("hidden", "");
+    scanningArea.removeAttribute('hidden');
+    const subscription = scanner.scanForQrCode().subscribe((room: Room) => {
+        joinRoom(room);
+        subscription.unsubscribe();
+    });
+  });
 
-    drawLine(code.location.topLeftCorner, code.location.topRightCorner);
-    drawLine(code.location.topRightCorner, code.location.bottomRightCorner);
-    drawLine(code.location.bottomRightCorner, code.location.bottomLeftCorner);
-    drawLine(code.location.bottomLeftCorner, code.location.topLeftCorner);
-    video.pause();
-    const room = JSON.parse(code.data) as Room;
-    joinRoom(room);
-}
-
-function drawLine(start: Point, end: Point) {
-    scannerContext.beginPath();
-    scannerContext.moveTo(start.x, start.y);
-    scannerContext.lineTo(end.x, end.y);
-    scannerContext.lineWidth = 4;
-    scannerContext.strokeStyle = 'yellow';
-    scannerContext.stroke();
+  connect.addEventListener("click", () => {
+    const idElement = document.getElementById("connectId") as HTMLInputElement;
+    const id = idElement.value;
+    joinRoom(new Room(id, "butts"));
+  });
 }
 
 function joinRoom(room: Room) {
-    if (!confirm('Is it ok to join room "' + room.name + '"?')) {
-        return;
-    }
-    console.log('conecting...');
-    telephone.connectTo(room).subscribe(() => {
-        console.log('connected');
-        preparePlayArea();
-    });
+  if (!confirm('Is it ok to join room "' + room.name + '"?')) {
+    return;
+  }
+  console.log("conecting...");
+  telephone.connectTo(room).subscribe(() => {
+    console.log("connected");
+    preparePlayArea();
+  });
 }
 
 function preparePlayArea() {
-    console.log('prepping play area');
-    const scanningArea = document.getElementById('scanningArea');
-    scanningArea.setAttribute('hidden','');
-    const playArea = document.getElementById('playArea');
-    playArea.removeAttribute('hidden');
-    waitForClues();
-    sendDrawing.addEventListener('click', () => {
-        const data = drawingBoard.toDataUrl();
-        const message = new NewDrawing(data);
-        telephone.SendMessage(message);
-        waitForStateChange(RoomState.WaitingForGuesses).subscribe(() => {
-            playArea.setAttribute('hidden','');
-            guessArea.removeAttribute('hidden');
-            submitFinalGuess();
-        });
-    })
+  console.log("prepping play area");
+  const scanningArea = document.getElementById("scanningArea");
+  scanningArea.setAttribute("hidden", "");
+  const playArea = document.getElementById("playArea");
+  playArea.removeAttribute("hidden");
+  waitForClues();
+  sendDrawing.addEventListener("click", () => {
+    const data = drawingBoard.toDataUrl();
+    const message = new NewDrawing(data);
+    telephone.SendMessage(message);
+    waitForStateChange(RoomState.WaitingForGuesses).subscribe(() => {
+      playArea.setAttribute("hidden", "");
+      guessArea.removeAttribute("hidden");
+      submitFinalGuess();
+    });
+  });
 }
 
 function waitForStateChange(expectedState: RoomState): Observable<void> {
-    const waiter = new Subject<void>();
-    const subscription = telephone.roomState.subscribe((state) => {
-        if (state != expectedState) {
-            console.log('skipping ',state);
-        }
-        waiter.next();
-        waiter.complete();
-    });
-    return waiter.asObservable();
+  const waiter = new Subject<void>();
+  const subscription = telephone.roomState.subscribe(state => {
+    if (state != expectedState) {
+      console.log("skipping ", state);
+    }
+    waiter.next();
+    waiter.complete();
+  });
+  return waiter.asObservable();
 }
 
 function waitForClues() {
-    console.log('waiting for clues');
-    const clueSub = telephone.clues.subscribe((clue: string) => {
-        alert('Your clue is: "'+clue+'"');
-        clueSub.unsubscribe();
-    });
+  console.log("waiting for clues");
+  const clueSub = telephone.clues.subscribe((clue: string) => {
+    alert('Your clue is: "' + clue + '"');
+    clueSub.unsubscribe();
+  });
 }
 
 function waitForNewRound() {
-    console.log('waiting for answer');
+  console.log("waiting for answer");
 }
 
 function submitFinalGuess() {
-    guessArea.removeAttribute('hidden');
-    const sendGuess = document.getElementById('sendGuess');
-    sendGuess.addEventListener('click', () => {
-        const guessElement = document.getElementById('guess') as HTMLInputElement;
-        const guess = new Guess(guessElement.value);
-        telephone.SendMessage(guess);
-        waitForNewRound();
-    });    
-}
-
-function startScanning() {
-    const mediaConstraints: MediaStreamConstraints = {
-        video: {
-            facingMode: 'environment'
-    }};
-    // use facingMode: environment to get front camera on phones
-    navigator.mediaDevices.getUserMedia(mediaConstraints)
-        .then((stream: MediaStream) => {
-            video.srcObject = stream;
-            // required for ios safari
-            video.setAttribute('playsinline', 'true');
-            video.play();
-            requestAnimationFrame(analyzeFrame);
-        });
-}
-
-window.onload = () => {
-    initialize();
-    startScanning();
+  guessArea.removeAttribute("hidden");
+  const sendGuess = document.getElementById("sendGuess");
+  sendGuess.addEventListener("click", () => {
+    const guessElement = document.getElementById("guess") as HTMLInputElement;
+    const guess = new Guess(guessElement.value);
+    telephone.SendMessage(guess);
+    waitForNewRound();
+  });
 }
---
+++ src/scanner.ts
@@ -0,0 +1,71 @@
+import jsQR, {QRCode} from 'jsqr';
+import { Point } from './point';
+import { Room } from './models/room';
+import { Subject, Observable } from 'rxjs';
+
+export class Scanner {
+    private readonly mediaConstraints: MediaStreamConstraints = {
+        video: {
+            facingMode: 'environment'
+    }};
+    
+    private video: HTMLVideoElement;
+    private scanner: HTMLCanvasElement;
+    private scannerContext: CanvasRenderingContext2D;
+    private scanSuccessful: Subject<Room>;
+
+    constructor(private elementId: string) {
+        this.scanSuccessful = new Subject<Room>();
+    }
+
+    public scanForQrCode(): Observable<Room> {
+        this.scanner = document.getElementById(this.elementId) as HTMLCanvasElement;
+        this.scannerContext = this.scanner.getContext('2d');
+        this.video = document.createElement('video') as HTMLVideoElement;
+        // use facingMode: environment to get front camera on phones
+        navigator.mediaDevices.getUserMedia(this.mediaConstraints)
+        .then((stream: MediaStream) => {
+            this.video.srcObject = stream;
+            // required for ios safari
+            this.video.setAttribute('playsinline', 'true');
+            this.video.play();
+            return requestAnimationFrame(() => this.analyzeFrame());
+        });
+        return this.scanSuccessful.asObservable();
+    }
+
+        private analyzeFrame(): number {
+        if (this.video.readyState !== this.video.HAVE_ENOUGH_DATA) {
+            return requestAnimationFrame(() => this.analyzeFrame());
+        }
+    
+        this.scanner.height = this.video.videoHeight;
+        this.scanner.width = this.video.videoWidth;
+        this.scannerContext.drawImage(this.video, 0, 0, this.scanner.width, this.scanner.height);
+        const imageData = this.scannerContext.getImageData(0, 0, this.scanner.width, this.scanner.height);
+        const code: QRCode = jsQR(imageData.data, imageData.width, imageData.height);
+        if (!code) {
+            return requestAnimationFrame(() => this.analyzeFrame());
+        }
+    
+        this.drawLine(code.location.topLeftCorner, code.location.topRightCorner);
+        this.drawLine(code.location.topRightCorner, code.location.bottomRightCorner);
+        this.drawLine(code.location.bottomRightCorner, code.location.bottomLeftCorner);
+        this.drawLine(code.location.bottomLeftCorner, code.location.topLeftCorner);
+        const streams = <MediaStream>this.video.srcObject;
+        streams.getTracks().forEach((track) => {
+            track.stop();
+        });
+        const room = JSON.parse(code.data) as Room;
+        this.scanSuccessful.next(room);
+    }
+
+    private drawLine(start: Point, end: Point): void {
+        this.scannerContext.beginPath();
+        this.scannerContext.moveTo(start.x, start.y);
+        this.scannerContext.lineTo(end.x, end.y);
+        this.scannerContext.lineWidth = 4;
+        this.scannerContext.strokeStyle = 'yellow';
+        this.scannerContext.stroke();
+    }
+}
