b5a8a54b92db53059b34c764f91c6cba460061b4
Mike Wilkinson <mwilkinson@imo-online.com>
2018-11-20 13:11:04 -0600

    Got beautiful sliding animations on host side, too.

--- src/components/component.ts
+++ src/components/component.ts
@@ -1,12 +1,29 @@
 export abstract class Component {
+    private _hide: EventListener;
+
     protected transitionTo(area: string) {
-        const allAreas = document.querySelectorAll('body > div');
+        const allAreas = document.querySelectorAll('.master-container > div');
         allAreas.forEach((value: Element) => {
             if (value.id == area) {
                 value.classList.remove('hidden');
+                value.classList.remove('slide-out');
+                value.classList.add('slide-in');
+            } else if (value.classList.contains('hidden')) {
+                return;
             } else {
-                value.classList.add('hidden');
+                value.classList.remove('slide-in');
+                value.classList.add('slide-out');
+                this._hide = this.hide.bind(this);
+                value.addEventListener('animationend', this._hide);
+                value.addEventListener('webkitAnimationEnd', this._hide);
             }
         });
+    }
+
+    private hide(event: Event) {
+        event.srcElement.classList.add('hidden');
+        event.srcElement.classList.remove('slide-out');
+        event.srcElement.removeEventListener('animationend', this._hide);
+        event.srcElement.removeEventListener('webkitAnimationEnd', this._hide);
     }
 }
